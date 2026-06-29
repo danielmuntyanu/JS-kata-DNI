4df34270089bbb31291641cd91daf1a26070f734
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-06-29 15:18:42 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import './style.css'
+import calculateLetter from './scripts/calculateLetter.js'
+
+
+document.querySelector('#app').innerHTML = `
+<section>
+  <div>
+    <h1>Kata - DNI</h1>
+    <p>Enter the number between 0 and 99999999 (up to 8 ciphers)</p>
+  </div>
+
+  <div class="panel">
+
+    <input 
+      type="text" 
+      id="numberInput" 
+      placeholder="0-99999999"
+    />
+
+    <button 
+      id="calculateBtn"
+    >
+      Calculate
+    </button>
+
+  </div>
+
+  <label
+      id="result"
+    >
+      T
+    </label>
+
+</section>
+`
